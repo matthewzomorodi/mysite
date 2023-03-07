8dc24be9af05f6bf0bbc6e2609a2ea75879af447
Matthew Zomorodi <matt@mhwk.team>
2023-03-07 12:41:59 -0500

    Add New Client Form

--- polls/views/__init__.py
+++ polls/views/__init__.py
@@ -1,2 +1,3 @@
 from .index import *
 from .question import *
+from .client import *
--- polls/views/client.py
+++ polls/views/client.py
@@ -6,6 +6,16 @@ def new_client(request):
 
     if request.method == 'GET':
         print('DEBUG=> views.client.new_client: method==GET')
+
+        context = {}
+        return render(request, 'polls/new_client.html', context)
     
     if request.method == 'POST':
-        print('DEBUG=> views.client.new_client: method==POST') 
+        print('DEBUG=> views.client.new_client: method==POST')
+        for key, val in request.POST.items():
+            print('DEBUG=> views.client.new_client: key==' + key)
+            print('DEBUG=> views.client.new_client: val==' + str(val))
+            print('DEBUG=> views.client.new_client: type(val)==' + str(type(val)))
+
+        context = {}
+        return render(request, 'polls/new_client.html', context)
